7db87cbe4c663b304ae80ff6ac51a09c53742e7e
Mureed Hussain <imhussain0086@gmail.com>
2022-01-30 12:06:46 +0500

    counter update
    counter update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Counter from "./Counter";
 import "./App.css";
-
+import { NewCount } from "./NewCount";
 const App = () => {
   return (
     <div>
       <Counter />
+      <NewCount />
     </div>
   );
 };
---
+++ src/NewCount.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Counter.css";
+
+export const NewCount = () => {
+  const [count, setCount] = useState(0);
+  const [reset, setReset] = useState(0);
+  const Counter = () => {
+    setCount(count + 1);
+  };
+
+  const Less = () => {
+    setCount(count - 1);
+  };
+
+  const Reset = () => {
+    setCount(reset);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={Counter}>Increase</button>
+      <button onClick={Less}>Dcrease</button>
+      <button onClick={Reset}>Reset</button>
+    </div>
+  );
+};
+
+// export default NewCount;
